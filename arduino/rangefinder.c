fc026a30af84a0af36fa3a6120b04e60786949a3
WoojinK <34810033+genie5741@users.noreply.github.com>
2020-01-13 17:01:40 +0900

    Create rangefinder.c

---
+++ arduino/rangefinder.c
@@ -0,0 +1,37 @@
+const int trigPin = 13;
+const int echoPin = 12;
+
+long duration;
+int distance;
+char temp;
+bool flag = 0;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  //Start The Monitor Serial 
+  Serial.begin(9600);
+}
+
+int get_distance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2; //cm
+  return distance;
+}
+
+
+void loop() {
+  if(Serial.available()) {
+    temp = Serial.read();
+    if (temp == '1') flag = 1;
+    else if (temp == '0') flag = 0;
+  }
+  Serial.print(distance);
+  Serial.println("CM");
+}
