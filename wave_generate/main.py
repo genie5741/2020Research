bddc03ea330d987da09bb202d51f3d50c9ec29b7
WoojinK <34810033+genie5741@users.noreply.github.com>
2020-06-23 16:39:29 +0900

    update: output to csv

--- wave_generate/main.py
+++ wave_generate/main.py
@@ -1,6 +1,7 @@
 from src.arduino import arduino
 from src.client import client
 from src.movemotor import movemotor
+import pandas as pd
 
 
 class server:
@@ -11,13 +12,17 @@ class server:
         self.ev3_client.input("motor = movemotor()")
         self.data = []
 
-    def start_experiment(self, speed, second):
+    def start_experiment(name, self, speed, second):
         self.arduino_client.on()
         self.ev3_client.input("motor.move_for_seconds(" + speed + "," + second + ")")
 
         print(self.ev3_client.read_output())  # it would be "Success"
 
-        self.data = self.arduino_client.read_output()
+        self.data += self.arduino_client.read_output()
+
+        dataframe = pd.DataFrame(self.data)
+        dataframe.to_csv(".\result\{0}.csv".format(name), header=False, index=False)
+
         self.arduino_client.off()
 
     def visualize_graph(self):
